3fa23fa07b53c9d9ad4f36dccc05955e91da2db3
bobuzy <bobuzytech@gmail.com>
2024-07-31 18:34:33 +0000

    Fix identation

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
-            user.get("id"): [{
+            us.get("id"): [{
                 "task": task.get("title"),
                 "completed": task.get("completed"),
-                "username": user.get("username")
+                "username": us.get("username")
             } for task in requests.get(url + "todos",
-                                    params={"userId": user.get("id")}).json()]
-            for user in users}, jsonfile)
+                                       params={"userId": us.get("id")}).json()]
+            for us in users}, jsonfile)
