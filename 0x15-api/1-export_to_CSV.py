8bb0280e2d703ac552e2de7ed452d8842b8f8a88
bobuzy <bobuzytech@gmail.com>
2024-07-31 17:29:30 +0000

    Export data from API into csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    emp_id = str(int(sys.argv[1]))
+    user = requests.get(url + "users/" + emp_id).json()
+    user_name = user.get("name")
+    task_dict = requests.get(url + "todos", params={"userId": emp_id}).json()
+
+    csv_file = "{}.csv".format(emp_id)
+
+    with open(csv_file, mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in task_dict:
+            writer.writerow([emp_id, user_name,
+                    task.get("completed"), task.get("title")])
