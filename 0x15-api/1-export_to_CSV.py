c0bafb650f4d3c77b6e9627ad745d6bbe15db7ab
bobuzy <bobuzytech@gmail.com>
2024-07-31 18:10:44 +0000

    Write into csvfile using dictionary form

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,8 +13,17 @@ if __name__ == "__main__":
     task_dict = requests.get(url + "todos", params={"userId": emp_id}).json()
 
     csv_file = "{}.csv".format(emp_id)
+    fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+                    "TASK_TITLE"]
 
     with open(csv_file, mode='w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+
         for task in task_dict:
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                emp_id, user_name, task.get("completed"), task.get("title")))
+            writer.writerow({
+                "USER_ID": emp_id,
+                "USERNAME": user_name,
+                "TASK_COMPLETED_STATUS": task['completed'],
+                "TASK_TITLE": task['title']
+            })
